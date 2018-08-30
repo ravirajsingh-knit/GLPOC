7af7307c0ce743fc00a1de40c6ce9e52b4b02c0d
Ravi Raj Singh <raviraj.singh@synapse.com>
2018-08-30 11:46:17 +0530

    All done

--- mod1/mod1.component.js
+++ mod1/mod1.component.js
@@ -33,29 +33,3 @@ angular.module("mod1").component('mod1Display',{
 
 
 
-
-// controller('mod1Ctrl',['$scope','DataFactory',function($scope,DataFactory) {
-//     $scope.editMode=false;
-//     $scope.employes=angular.copy(DataFactory.getNames());
-
-//    console.log($scope.employes);
-//    $scope.changeMode=function(){
-//     $scope.editMode=!$scope.editMode;
-//    }
-//    $scope.clickedCancel=function(id){
-//     $scope.changeMode();
-//     $scope.employe=angular.copy(DataFactory.getUserData(id));
-//     console.log($scope.employe);
-//    }
-//    $scope.clickedSubmit=function(data){
-//     $scope.changeMode();
-//     $scope.employe=angular.copy(DataFactory.setData(data));
-//     console.log($scope.employe);
-//    }
-
-//    $scope.userData=function(id){
-//     $scope.editMode=false;
-//     $scope.employe=angular.copy(DataFactory.getUserData(id));
-//     console.log($scope.employe);
-//    }
-// }]);
--- mod2/mod2.component.js
+++ mod2/mod2.component.js
@@ -2,95 +2,112 @@ angular.module('mod2').component('mod2Display',{
     templateUrl:'mod2/mod2.template.html',
     controller:[ '$scope', 'DataFactory',
     function($scope,DataFactory){
+        var status=[];
         $scope.employes=angular.copy(DataFactory.getAllData());
-        $scope.showdata=0;
-        $scope.editdata=0;
+        for(var i=0;i<$scope.employes.length;i++){
+            status.push(0);
+            $scope.employes[i].status=0;
+        }
+
         $scope.delete=function(id){
             console.log("delete "+id);
-            $scope.employes=DataFactory.delete(id);
+            //$scope.employes=DataFactory.delete(id);
+            for(var i=0;i<$scope.employes.length;i++){
+                if($scope.employes[i].empid==id){
+                    $scope.employes.splice(i,1);
+                    break;
+                }
+            }
             console.log($scope.employes);
         };
+
         $scope.edit=function(id){
-            $scope.employes=angular.copy(DataFactory.getAllData());
-            if($scope.editdata==id){
-                $scope.showdata=0;
-                $scope.editdata=0;
-            }
-            else{
-                $scope.showdata=0;
-                $scope.editdata=id;
+            var employe=angular.copy(DataFactory.getUserData(id));
+            for(var i=0;i<$scope.employes.length;i++){
+                if($scope.employes[i].empid==id){
+                    $scope.employes[i]=angular.copy(employe);
+                    $scope.employes[i].status=2;
+                    status[i]=2;
+                }
+                
             }
         };
+
+
         $scope.viewData=function(id){
-            $scope.employes=angular.copy(DataFactory.getAllData());
-            if($scope.showdata==id){
-                $scope.editdata=0;
-                $scope.showdata=0;
-            }
-            else{
-                $scope.editdata=0;
-                $scope.showdata=id;
+            var employe=angular.copy(DataFactory.getUserData(id));
+            for(var i=0;i<$scope.employes.length;i++){
+                if($scope.employes[i].empid==id){
+                    $scope.employes[i]=angular.copy(employe);
+                    if(status[i]==1){
+                        $scope.employes[i].status=0;
+                        status[i]=0;
+                    }
+                    else{
+                        $scope.employes[i].status=1;
+                        status[i]=1;
+                    }
+                }
             }
         };
-    
-    
+
         $scope.editData=function(data){
-          //  console.log(data);
-            DataFactory.setData(data)
-            $scope.employes=angular.copy(DataFactory.getAllData());
-            $scope.showdata=data.empid;
-            $scope.editdata=0;
-         //   console.log(employes);
-        }
+            //  console.log(data);
+              DataFactory.setData(data)
+              var employe=angular.copy(DataFactory.getUserData(data.empid));
+            for(var i=0;i<$scope.employes.length;i++){
+                if($scope.employes[i].empid==data.empid){
+                    $scope.employes[i]=angular.copy(employe);
+                    $scope.employes[i].status=1;
+                    status[i]=1;
+                }
+             }
+
+          }
+
+
+
+
+
+
+
+
+        // $scope.showdata=0;
+        // $scope.editdata=0;
+        
+        // $scope.edit=function(id){
+        //     $scope.employes=angular.copy(DataFactory.getAllData());
+        //     if($scope.editdata==id){
+        //         $scope.showdata=0;
+        //         $scope.editdata=0;
+        //     }
+        //     else{
+        //         $scope.showdata=0;
+        //         $scope.editdata=id;
+        //     }
+        // };
+        // $scope.viewData=function(id){
+        //     $scope.employes=angular.copy(DataFactory.getAllData());
+        //     if($scope.showdata==id){
+        //         $scope.editdata=0;
+        //         $scope.showdata=0;
+        //     }
+        //     else{
+        //         $scope.editdata=0;
+        //         $scope.showdata=id;
+        //     }
+        // };
+    
+    
+        // $scope.editData=function(data){
+        //   //  console.log(data);
+        //     DataFactory.setData(data)
+        //     $scope.employes=angular.copy(DataFactory.getAllData());
+        //     $scope.showdata=data.empid;
+        //     $scope.editdata=0;
+        //  //   console.log(employes);
+        // }
     
     }
     ]
 });
-
-
-
-
-// app.controller('mod2Ctrl',['$scope','DataFactory',function($scope,DataFactory) {
-//     $scope.employes=angular.copy(DataFactory.getAllData());
-//     $scope.showdata=0;
-//     $scope.editdata=0;
-//     $scope.delete=function(id){
-//         console.log("delete "+id);
-//         $scope.employes=DataFactory.delete(id);
-//         console.log($scope.employes);
-//     };
-//     $scope.edit=function(id){
-//         $scope.employes=angular.copy(DataFactory.getAllData());
-//         if($scope.editdata==id){
-//             $scope.showdata=0;
-//             $scope.editdata=0;
-//         }
-//         else{
-//             $scope.showdata=0;
-//             $scope.editdata=id;
-//         }
-//     };
-//     $scope.viewData=function(id){
-//         $scope.employes=angular.copy(DataFactory.getAllData());
-//         if($scope.showdata==id){
-//             $scope.editdata=0;
-//             $scope.showdata=0;
-//         }
-//         else{
-//             $scope.editdata=0;
-//             $scope.showdata=id;
-//         }
-//     };
-
-
-//     $scope.editData=function(data){
-//       //  console.log(data);
-//         DataFactory.setData(data)
-//         $scope.employes=angular.copy(DataFactory.getAllData());
-//         $scope.showdata=data.empid;
-//         $scope.editdata=0;
-//      //   console.log(employes);
-//     }
-
-
-// }]);
